4d8360326af54e076a7ac3038137c6fc006328b5
nguyenhuyit2308 <huyreunglang123@gmail.com>
2025-03-25 09:16:30 +0700

    update file register and update

--- resources/views/crud_user/read.blade.php
+++ resources/views/crud_user/read.blade.php
@@ -1,27 +1,39 @@
-
 @extends('dashboard')
 
 @section('content')
-    <main class="login-form">
-        <div class="container">
-            <div class="row justify-content-center">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{{$messi->id}}</td>
-                            <td>{{$messi->name}}</td>
-                            <td>{{$messi->email}}</td>
-                        </tr>
-                    </tbody>
-                </table>
+<main class="login-form py-4">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow-lg">
+                    <div class="card-header bg-primary text-white text-center">
+                        <h4>Thông Tin Người Dùng</h4>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered text-center">
+                            <thead class="table-dark">
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Phone</th>
+                                    <th>Address</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $messi->id }}</td>
+                                    <td>{{ $messi->name }}</td>
+                                    <td>{{ $messi->phone }}</td>
+                                    <td>{{ $messi->address }}</td>
+                                    <td>{{ $messi->email }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
-    </main>
+    </div>
+</main>
 @endsection
